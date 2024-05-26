3ac2a0d2969229d4c97a0e99807d57818c97de20
CStage <c.stage@live.dk>
2024-05-26 12:21:41 +0200

    add functionality for copying PR templates from TK and review requests from Azure DevOps

--- popup.js
+++ popup.js
@@ -36,13 +36,27 @@ async function GrabLink(copyTemplate)
         onlyId.style.visibility = "visible";
 
     }
+    
     const resp = await fetch(activeTabUrl);
     var text = await resp.text();
     var parser = new DOMParser();
     var doc = parser.parseFromString(text, 'text/html');
     var title = doc.querySelectorAll("title")[0];
     let identifier = title.innerText;
-    if (copyTemplate){
+    if (activeTabUrl.includes("source.netcompany.com/tfs/Netcompany/")){
+        identifier = activeTabUrl.split("ANS_")[1].split("/pullrequest")[0] + " PR"
+        copyAsReviewRequest(activeTabUrl, identifier)
+    }
+    else if (activeTabUrl.includes("goto.netcompany.com")){
+        let breadCrumbContainingTitle = doc.getElementsByClassName("ms-breadcrumbCurrentNode")[0]
+        identifier = breadCrumbContainingTitle.innerText;
+
+        let trimmedUrl = activeTabUrl.split("&")[0]
+
+        let prTemplate = `[${identifier}](${trimmedUrl})`;
+        copyAsPrTemplate(prTemplate)
+    }
+    else if (copyTemplate){
         let prTemplate = `[${identifier}](${activeTabUrl})`;
         copyAsPrTemplate(prTemplate)
     }
@@ -50,6 +64,44 @@ async function GrabLink(copyTemplate)
         copyAsHyperlink(activeTabUrl, identifier);
     }
     
+}
+
+function copyAsReviewRequest(url, title) {
+     var div = document.createElement('div');
+    var anchor = document.createElement('a');
+    var beginningParagraph = document.createElement("span");
+    var endingParagraph = document.createElement("span");
+    
+    beginningParagraph.innerHTML = "@PR I have a ";
+    endingParagraph.innerHTML = " ready for review.";
+    anchor.setAttribute('href', url);
+    anchor.innerHTML = title;
+    
+    div.appendChild(beginningParagraph);
+    div.appendChild(anchor);
+    div.appendChild(endingParagraph);
+    
+    div.style.fontFamily = 'Calibri';
+    div.style.fontSize = '11pt';
+    div.style.backgroundColor = 'transparent';
+    
+    document.body.appendChild(div);
+    
+    var range = document.createRange();
+    range.selectNodeContents(div);
+    var selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+    
+    try {
+        document.execCommand('copy');
+        console.log('Copied: ', div.innerHTML);
+    } catch (err) {
+        console.error('Failed to copy: ', err);
+    }
+    
+    selection.removeAllRanges();
+    document.body.removeChild(div);
 }
 
 function copyAsHyperlink(url, title) {
